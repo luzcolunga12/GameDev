5a9d89f88cbc08ba0cda2798c3e7f77fe644cdeb
Luz Colunga <luz.colunga@udem.edu>
2023-06-02 23:58:25 -0600

    commit

---
+++ DeliveryGame/Assets/Collision.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+   {
+
+        Debug.Log("Boom!");
+
+   }
+
+    void OnTriggerEnter2D(Collider2D other) {
+     Debug.Log("What was that?");
+   }
+}
