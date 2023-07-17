44f69fefaa57f35410c36b53b28b62f920402916
Luz Colunga <luz.colunga@udem.edu>
2023-07-16 23:59:52 -0600

    git

--- DeliveryGame/Assets/Delivery.cs
+++ DeliveryGame/Assets/Delivery.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
     [SerializeField] float destroyDelay = 0.5f;
     bool hasPackage;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() {
+        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
 
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -18,6 +32,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package picked up!");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, destroyDelay);
         }
 
@@ -25,6 +40,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package Delivered!");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor; 
         }
     }
 }
